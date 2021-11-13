3f45d0c2c9a9ba3f1cbb339542eab0e3736457d8
kristofajosh <christopherjoshua18@live.com>
2021-11-13 11:52:42 +0100

    refactor: api call did not read from json after first call.

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,22 @@
+import {resolveKittenResponse} from "./utility";
+
 /**
- * Fetch kittens from random user api server
+ * Fetch kittens - if local json is empty trigger online kittens, save then load from local onwards
  * {@link https://randomuser.me/documentation}
- * @type {Promise<{results: Array<{name: {first: string, last: string}, login:{uuid: string}, location: {street:{number: number}}}>}>}
  */
-export const fetchKittens  = fetch('https://randomuser.me/api/?inc=name,location,picture,login&results=100&nat=gb').then(r=>r.json())
+export const fetchKittens = async () => {
+  const localKittens = await fetch('/server/kittens.json').then(r => r.json())
+  if(!localKittens.kittens.data.length) {
+    const onlineKittens = await fetch('https://randomuser.me/api/?inc=name,location,picture,login&results=100&nat=gb').then(r => r.json())
+    return new Promise((resolve, reject) => {
+      fetch('http://localhost:8080/kittens', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({data: onlineKittens.results})
+      }).then(r => r.json()).then(resolve).catch(()=>{
+        resolve({data: onlineKittens.results})
+      })
+    })
+  }
+  return localKittens.kittens
+}
